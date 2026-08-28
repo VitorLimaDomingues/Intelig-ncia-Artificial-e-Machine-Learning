f469178eb7f4e549c172dd7a90a08576c8b6565e
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2026-08-28 11:24:18 -0300

    Adiciona exercicios.py

---
+++ Aula-02/exercicio.py
@@ -0,0 +1,6 @@
+# 1)Escreva um programa que receba uma frase e agrupe as palavras em um dicionário de acordo com a quantidade de letras que cada uma possui.
+# As chaves do dicionário devem ser o  tamanho da palavra (inteiro) e os valores devem ser listas contendo as palavras daquele tamanho.
+
+
+# 2) Crie uma função chamada atualizar_estoque que receba um dicionário representando  o estoque atual de produtos e uma lista de vendas realizadas (onde cada venda é uma tupla com o nome do produto e a quantidade vendida).  
+#A função deve subtrair a quantidade vendida do estoque e  emitir um aviso caso algum produto não possua quantidade suficiente para cobrir a venda.
