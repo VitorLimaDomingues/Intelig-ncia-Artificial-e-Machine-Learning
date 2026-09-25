0f5e3ed878c4f1484fcbe6400206582f52cdd3f1
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2026-09-25 13:02:04 -0300

    feat(python): Codifica tabela para o PDF

--- Aula-05/src/python/main.py
+++ Aula-05/src/python/main.py
@@ -17,3 +17,14 @@ def gerar_pdf(filename="relatorio.pdf"):
         ["Horas Perdidas(Bugs)", "4.5H"],
         ["Valor Líquido Gerado"]
     ]
+
+    tabela = table(dados,colWidths=[250, 250])
+    tabela.setStyle(TableStyle([
+        ('BACKGROUND', (0,0), (-1,0), colors.HexColor("#1D293B")),
+        ('TEXTCOLOR', (0,0), (-1,0), colors.white),
+        ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
+        ('GRID', (0,0), (-1,0), colors.HexColor("#acbdSe1")),
+        ('ROWBACKGROUND', (0,0), (-1, 0), [colors.white, colors.HexColor("#F8FAFC")])
+    ]))
+
+    story.append(tabela)
